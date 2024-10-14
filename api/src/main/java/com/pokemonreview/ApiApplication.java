b96dac061c1f7f41c847e542eab57fc1a088bd66
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2024-10-14 13:41:17 +0200

    Controller and postman test

--- api/src/main/java/com/pokemonreview/ApiApplication.java
+++ api/src/main/java/com/pokemonreview/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.pokemonreview.api;
+package com.pokemonreview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
